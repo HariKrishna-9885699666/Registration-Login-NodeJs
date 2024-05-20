e242e9c6a892ddbd95eead44756ec9a9e37e5bf1
Hari Krishna Anem <anemharikrishna@gmail.com>
2024-05-20 12:36:33 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -24,6 +24,7 @@ module.exports = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    mysqlPort: process.env.MYSQL_PORT,
     dialect: "mysql",
     logging: true,
   },
